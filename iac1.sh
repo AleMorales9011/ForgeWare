398d300ef5e330fbef9cc312f0d50289c805ee72
AleMorales9011 <alemorales9011@gmail.com>
2024-03-17 09:22:20 +0000

    added iac1.sh

---
+++ iac1.sh
@@ -0,0 +1,17 @@
+#!bin/bash
+----------------------------------------------------------------------------------
+echo "Atualizando e instalando apache2 e unzip..."
+apt-get update
+apt-get upgrade -y
+apt-get install apache2 -y
+apt-get install unzip -y
+----------------------------------------------------------------------------------
+echo "Baixando site de repositorio remoto..."
+cd /tmp
+wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
+----------------------------------------------------------------------------------
+echo "Descomprimindo arquivo e copiando na pasta do servidor web..."
+unzip main.zip
+cd linux-site-dio-main
+cp -R * /var/www/html/
+----------------------------------------------------------------------------------
